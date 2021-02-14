91f18330560a8cce757cd0e13948cc606123a798
Luke <luke@yorkbay.net.nz>
2021-02-15 11:01:19 +1300

    added file inout and giver to rust

--- www/index.js
+++ www/index.js
@@ -4,3 +4,33 @@ wasm.greet()
 
 let data = new Uint8Array([21,31])
 wasm.toAscii(data)
+
+let input = document.getElementById("image-input")
+
+function readFile(input) {
+    let file = input.files[0]
+
+    let reader = new FileReader()
+
+    reader.readAsArrayBuffer(file)
+
+    let bytes
+
+    reader.onload = () => {
+        console.log("Loaded reader")
+        bytes = reader.result
+        console.log("Bytes: " + bytes)
+
+        let newData = new Uint8Array(bytes)
+        wasm.toAscii(newData)
+
+    }
+
+    reader.onerror = () => {
+        console.log("reader.error")
+    }
+}
+
+input.addEventListener("change", (event) => {
+    readFile(event.target)
+})
